eda6474a282b7abd4f14b5820120605a05d6f6fe
LimjeongSik <ol37@naver.com>
2023-10-09 20:26:05 +0900

    feature : replace PostApi to api and login test

--- src/apis/api/posts/index.ts
+++ src/apis/api/posts/index.ts
@@ -1,39 +1,39 @@
-import { PostApi } from "../../instance.api";
+import { api } from "../../instance.api";
 
 // 타입
 import { PatchPost, Post } from "@/interface";
 
 // 포스트 가져오기
 export async function getPosts() {
-    const response = await PostApi.get("/posts");
+    const response = await api.get("/posts");
 
     return response.data as Post[];
 }
 
 // 특정 포스트 가져오기
 export async function getPost(id: number) {
-    const response = await PostApi.get(`/posts/${id}`);
+    const response = await api.get(`/posts/${id}`);
 
     return response.data as Post;
 }
 
 // 포스트 생성하기
 export async function createPost(post: Post) {
-    const response = await PostApi.post("/posts", post);
+    const response = await api.post("/posts", post);
 
     return response.data as Post;
 }
 
 // 포스트 수정하기
 export async function patchPost(id: number, post: PatchPost) {
-    const response = await PostApi.patch(`/posts/${id}`, post);
+    const response = await api.patch(`/posts/${id}`, post);
 
     return response.data as Post;
 }
 
 // 포스트 삭제하기
 export async function deletePost(id: number) {
-    const respone = await PostApi.delete(`/posts/${id}`);
+    const respone = await api.delete(`/posts/${id}`);
 
     return respone.data as Post[];
 }
--- src/apis/instance.api.ts
+++ src/apis/instance.api.ts
@@ -2,6 +2,9 @@ import axios, { AxiosRequestConfig } from "axios";
 
 const axiosConfig: AxiosRequestConfig = {
     baseURL: "https://jsonplaceholder.typicode.com",
+    headers: {
+        "Content-Type": "application/json",
+    },
 };
 
-export const PostApi = axios.create(axiosConfig);
+export const api = axios.create(axiosConfig);
