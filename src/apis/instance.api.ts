46e0d2d6934c35259a6945f34970d3e5005e9dd1
jarvis1543 <pjy4952@naver.com>
2023-11-09 18:56:04 +0900

    update asios baseURL

--- src/apis/instance.api.ts
+++ src/apis/instance.api.ts
@@ -1,9 +1,9 @@
-import axios, { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from 'axios';
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: "http://localhost:8000",
+  baseURL: 'http://pjy4952.cafe24.com:8000',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 };
 
